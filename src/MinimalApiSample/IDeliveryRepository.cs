ad1802e6a7429ccb3be45d30998040eeb316f864
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-27 04:01:17 -0800

    add repository

---
+++ src/MinimalApiSample/IDeliveryRepository.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace MinimalApiSample;
+
+public interface IDeliveryRepository
+{
+  public Task<DeliveryEntity?> GetDeliveryAsync(Guid id, CancellationToken cancellationToken);
+
+  public Task AddDeliveryAsync(DeliveryEntity delivery, CancellationToken cancellationToken);
+}
