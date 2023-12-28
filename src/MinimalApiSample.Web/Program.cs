dd2e60787e2010ba262f0bd088fe679c9692db4d
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-28 05:12:35 -0800

    add add delivery handler extension

--- src/MinimalApiSample.Web/Program.cs
+++ src/MinimalApiSample.Web/Program.cs
@@ -2,16 +2,8 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using MinimalApiSample;
-using MinimalApiSample.Web;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
-app.MapPost("/api/delivery", async (AddDeliveryRequestDto requestDto, IDeliveryRepository deliveryRepository, CancellationToken cancellationToken) =>
-{
-  ArgumentNullException.ThrowIfNull(requestDto);
-  await deliveryRepository.AddDeliveryAsync(requestDto.ToEntity(), cancellationToken);
-  return Results.Created();
-});
+app.MapAddDeliveryHandler();
 app.Run();
