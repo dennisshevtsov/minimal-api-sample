c828a3e2b1a22863f2a468f1682853e303174ee1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-28 05:01:36 -0800

    add add delivery endpoint

--- src/MinimalApiSample.Web/Program.cs
+++ src/MinimalApiSample.Web/Program.cs
@@ -2,8 +2,16 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using MinimalApiSample;
+using MinimalApiSample.Web;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.MapPost("/api/delivery", async (AddDeliveryRequestDto requestDto, IDeliveryRepository deliveryRepository, CancellationToken cancellationToken) =>
+{
+  ArgumentNullException.ThrowIfNull(requestDto);
+  await deliveryRepository.AddDeliveryAsync(requestDto.ToEntity(), cancellationToken);
+  return Results.Created();
+});
 app.Run();
