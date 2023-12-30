6b5f289e9104f41d4e0f8e37486764412bdf6ac1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-30 02:57:20 -0800

    add extention to register in memory delivery repository

---
+++ src/MinimalApiSample.Data/InMemoryDataServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using MinimalApiSample;
+using MinimalApiSample.Data;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class InMemoryDataServicesExtensions
+{
+  public static IServiceCollection AddInMemoryData(this IServiceCollection services)
+  {
+    ArgumentNullException.ThrowIfNull(services);
+    services.AddSingleton<IDeliveryRepository, InMemoryDeliveryRepository>();
+    return services;
+  }
+}
